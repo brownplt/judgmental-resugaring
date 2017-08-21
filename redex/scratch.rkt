8203386a1647717bf8fe703fb88e745a38d2f342
Justin <zallambo@gmail.com>
2017-08-21 16:09:12 -0400

    A clean resugaring of let. Resugaring split in two phases: find derivation & unification.

--- redex/scratch.rkt
+++ redex/scratch.rkt
@@ -1,6 +1,7 @@
 #lang racket
 
 (require redex)
+(require "unify.rkt")
 
 (provide (all-defined-out))
 
@@ -15,8 +16,8 @@
      (e e)
      (pair e e)
      (+ e e)
-     n
-     (atom x)) ;meta
+     (if e e e)
+     n)
   (p ::=
      x
      (pair p p))
@@ -25,169 +26,110 @@
      (pair v v)
      n)
   (n ::= number)
-  (x ::= variable-not-otherwise-mentioned)
+  (x ::= variable-not-otherwise-mentioned) ; fixed
   (t ::=
      (t -> t)
      (Pair t t)
-     N
-     x) ;meta
-  (Γ ::= [(x : t) ...])
-  ; meta:
-  (φ ::= ([j ...] [q ...]))
-  (j ::=  (Γ ⊢ (atom x) : t))
+     Num
+     Bool)
+  (Γ ::= [(x : t) ...])) ; fixed
+
+(define-extended-language the-meta-syntax the-syntax ; fixed
+  (e ::= ....
+     (atom x : x))
+  (t ::= ....
+     x)
+  (c ::=
+     j
+     q)
+  (j ::=  (Γ ⊢ (atom x : x) : t))
   (q ::= (t = t)))
 
 
 ;; ---------------------------------------------------------------------------------------------------
 ;; typing
 
-(define-metafunction the-syntax
-  lookup : x Γ -> t
-  [(lookup x [(x_1 : t_1) ... (x : t) (x_2 : t_2) ...])
-   t
-   (side-condition (not (member (term x) (term (x_2 ...)))))])
-
-(define-metafunction the-syntax
+(define-metafunction the-meta-syntax
   extend : x t Γ -> Γ
   [(extend x t [(x_s : t_s) ...])
    [(x_s : t_s) ... (x : t)]])
 
-(define-metafunction the-syntax
-  lookup-eq : x [q ...] -> t or #f
-  [(lookup-eq x [(x_1s = t_1s) ... (x = t) (x_2s = t_2s) ...])
-   t]
-  [(lookup-eq x [q ...])
-   #f])
-
-(define-metafunction the-syntax
-  lookup-judge : x [j ...] -> j
-  [(lookup-judge x [(Γ_1 ⊢ (atom x_1) : t_1) ...
-                    (Γ   ⊢ (atom x)   : t)
-                    (Γ_2 ⊢ (atom x_2) : t_2) ...])
-   (Γ ⊢ (atom x) : t)])      
-
-(define-metafunction the-syntax
-  equate : t t [q ...] -> [q ...]
-  [(equate x t [q ...])
-   (equate t_2 t [q ...])
-   (where t_2 (lookup-eq x [q ...]))]
-  [(equate x t [(x_s = t_s) ...])
-   ; Maintain the invarient that across eqs, lhs not in rhs, and lhs all distinct.
-   ; If we're in this case, then (i) x not in lhs[q ...], (ii) x not in t.
-   ; Perform substitutions to also ensure that lhs[q ...] not in t, and x not in rhs[q ...].
-   [(x = (subs-eq [(x_s = t_s) ...] t)) (x_s = (subs-eq [(x = t)] t_s)) ...]
-   (judgment-holds (not-contains t x))]
-  ; symmetric cases
-  [(equate t x [q_s ...])
-   (equate x t [q_s ...])]
-  ; language-specific
-  [(equate (t_1 -> t_2) (t_3 -> t_4) [q_s ...])
-   (equate t_1 t_3 (equate t_2 t_4 [q_s ...]))]
-  [(equate (Pair t_1 t_2) (Pair t_3 t_4) [q_s ...])
-   (equate t_1 t_3 (equate t_2 t_4 [q_s ...]))]
-  [(equate N N [q_s ...])
-   [q_s ...]])
-
-(define-metafunction the-syntax
-  subs-eq : [q ...] t -> t
-  ; language-specific
-  [(subs-eq [q ...] (t_1 -> t_2))
-   ((subs-eq [q ...] t_1) -> (subs-eq [q ...] t_2))]
-  [(subs-eq [q ...] (Pair t_1 t_2))
-   (Pair (subs-eq [q ...] t_1) (subs-eq [q ...] t_2))]
-  [(subs-eq [q ...] N)
-   N]
-  [(subs-eq [(x_1s = t_1s) ... (x = t) (x_2s = t_2s) ...] x)
-   t]
-  [(subs-eq [q ...] x)
-   x])
-
-(define-metafunction the-syntax
-  subs-judge : [q ...] j -> j
-  [(subs-judge [q ...] (Γ ⊢ (atom x) : t))
-   (Γ ⊢ (atom x) : (subs-eq [q ...] t))])
-
-(define-metafunction the-syntax
-  add-eq : x t φ -> φ
-  [(add-eq x t ([j_s ...] [q_s ...]))
-   ([(subs-judge [q_2s ...] j_s) ...] [q_2s ...])
-   (where [q_2s ...] (equate x t [q_s ...]))])
-
-(define-metafunction the-syntax
-  add-judge : Γ x t φ -> φ
-  ; TODO: unification
-  [(add-judge Γ x t ([j_s ...] [q_s ...]))
-   ([(Γ ⊢ (atom x) : (subs-eq [q_s ...] t)) j_s ...] [q_s ...])])
-
-(define-metafunction the-syntax
-  union : φ φ -> φ
-  [(union φ ([(Γ ⊢ (atom x) : t) j_s ...] [q_s ...]))
-   (union (add-judge Γ x t φ) ([j_s ...] [q_s ...]))]
-  [(union φ ([] [q q_s ...]))
-   (union (add-eq q φ) ([] [q_s ...]))]
-  [(union φ ([] []))
-   φ])
-
-; language-specific
-(define-judgment-form the-syntax
-  #:contract (not-contains t x)
-  #:mode (not-contains I I)
-
-  [(not-contains t_1 x)
-   (not-contains t_2 x)
-   ------ contains-lambda
-   (not-contains (t_1 -> t_2) x)]
-
-  [(not-contains t_1 x)
-   (not-contains t_2 x)
-   ------ contains-pair-1
-   (not-contains (Pair t_1 t_2) x)]
-
-  [(side-condition ,(not (symbol=? (term x_1) (term x_2))))
-   ------ contains-var
-   (not-contains x_1 x_2)])
-
-(define-judgment-form the-syntax
-  #:contract (ty Γ e t φ)
-  #:mode (ty I I O O)
+(define-judgment-form the-meta-syntax
+  #:contract (ty Γ e t)
+  #:mode (ty I I O)
   
-  [------ t-atom
-   (ty Γ (atom x) x (add-judge Γ x x ([] [])))]
+  [(con (Γ ⊢ (atom x_1 : x_2) : x_2))
+   (side-condition ,(freshless? (term Γ)))
+   ------ t-atom
+   (ty Γ (atom x_1 : x_2) x_2)]
+
+  [(where Γ_2 ,(unfreshen (term Γ)))
+   (side-condition ,(not (freshless? (term Γ))))
+   (ty Γ_2 (atom x_1 : x_2) x_2)
+   ------ t-fresh
+   (ty Γ (atom x_1 : x_2) x_2)]
   
   [------ t-number
-   (ty Γ n N [])]
+   (ty Γ n N)]
 
   [(where t (lookup x Γ))
    ------ t-id
-   (ty Γ x t [])]
+   (ty Γ x t)]
 
-  [(ty Γ e_1 N φ_1)
-   (ty Γ e_2 N φ_2)
+  [(ty Γ e_1 Num)
+   (ty Γ e_2 Num)
    ------ t-plus
-   (ty Γ (+ e_1 e_2) N (union φ_1 φ_2))]
+   (ty Γ (+ e_1 e_2) Num)]
+
+  [(ty Γ e_1 Bool)
+   (ty Γ e_2 t_1)
+   (ty Γ e_3 t_2)
+   (con (t_1 = t_2))
+   ------ t-if
+   (ty Γ (if e_1 e_2 e_3) t_1)]
   
-  [(ty Γ e_1 t_1 φ_1)
-   (ty Γ e_2 t_2 φ_2)
+  [(ty Γ e_1 t_1)
+   (ty Γ e_2 t_2)
    ------ t-pair
-   (ty Γ (pair e_1 e_2) (Pair t_1 t_2) (union φ_1 φ_2))]
+   (ty Γ (pair e_1 e_2) (Pair t_1 t_2))]
   
-  [(ty Γ e_fun (t_1 -> t_3) φ_1)
-   (ty Γ e_arg t_2 φ_2)
+  [(ty Γ e_fun (t_1 -> t_3))
+   (ty Γ e_arg t_2)
+   (con (t_1 = t_2))
    ------ t-apply
-   (ty Γ (e_fun e_arg) t_3 (add-eq t_2 t_1 (union φ_1 φ_2)))]
+   (ty Γ (e_fun e_arg) t_3)]
   
-  [(ty (extend x t Γ) e t_2 φ)
+  [(ty (extend x t Γ) e t_2)
    ------ t-lambda
-   (ty Γ (λ (x : t) e) (t -> t_2) φ)])
-  
+   (ty Γ (λ (x : t) e) (t -> t_2))])
 
 
 
+;; ---------------------------------------------------------------------------------------------------
+;; meta-typing
 
-(define-syntax-rule (derive expr)
-  (show-derivations (build-derivations (ty [] expr _ _))))
+(define-judgment-form the-meta-syntax
+  #:contract (con c)
+  #:mode (con I)
+  [------ t-equal
+   (con q)]
+  [------ t-judge
+   (con j)])
 
-(derive ((λ (x : T) (atom B)) (atom A)))
-;(derive (+ (atom A) 1))
-;(derive (atom A))
 
+(define rule_let
+  (rule "let" #:fresh ()
+        (let x : T = (atom a : A) in (atom b : B))
+        ((λ (x : T) (atom b : B)) (atom a : A))))
+(define rule_thunk
+  (rule "thunk" #:fresh (x)
+        (thunk (atom a : A))
+        (λ (x : Num) (atom a : A))))
+(define rule_or
+  (rule "or" #:fresh (x)
+        (or (atom a : A) (atom b : B))
+        (if (atom a : A) (atom a : A) (atom b : B))))
+        ;((λ (x : A) (if x x (atom b : B))) (atom a : A))))
+(define the-literals (set 'λ ': '+ 'pair '-> 'Pair '= 'in))
+(define the-deriv (resugar rule_let ty the-literals))
+(show-derivations (list the-deriv))
---
+++ redex/unify.rkt
@@ -0,0 +1,297 @@
+#lang racket
+
+(require redex)
+
+(provide resugar freshless? unfreshen (rename-out (make-rule rule)))
+
+;; assumption: Redex model does not contain #f
+
+;; ------------------------------------------------------------
+;; Desugaring Rules
+
+(define fresh-vars (make-parameter #f))
+
+(define (fresh-binding? binding)
+  (boolean (member (car binding) (fresh-vars))))
+
+(define (unfreshen Γ)
+  (filter (λ (b) (not (fresh-binding? b))) Γ))
+
+(define (freshless? Γ)
+  (andmap (λ (b) (not (fresh-binding? b))) Γ))
+
+(define-struct ds-rule (name fresh lhs rhs))
+
+(define-syntax-rule (make-rule name #:fresh (fresh-name ...) lhs rhs)
+  (ds-rule name (list 'fresh-name ...) (term lhs) (term rhs)))
+
+
+;; ------------------------------------------------------------
+;; Language Literals
+
+(define language-literals (make-parameter #f))
+
+(define (variable? x)
+  (and (symbol? x)
+       (not (set-member? (language-literals) x))))
+
+(define (literal? x)
+  (and (symbol? x)
+       (set-member? (language-literals) x)))
+
+
+;; ------------------------------------------------------------
+;; Judgements and Equations
+
+(define (print-judgement j port mode)
+  (show (format "  ~a ⊢ ~a : ~a\n" (judgement-env j) (judgement-id j) (judgement-type j))
+        port mode))
+
+(define (print-equation eq port mode)
+  (show (format "  ~a = ~a\n" (equation-left eq) (equation-right eq))
+        port mode))
+
+(define-struct judgement (env id type)
+  #:methods gen:custom-write
+  [(define write-proc print-judgement)])
+
+(define-struct equation (left right)
+  #:methods gen:custom-write
+  [(define write-proc print-equation)])
+
+(define (read-eq eq)
+  (match eq
+    [(list a '= b)
+     (equation a b)]
+    [(list Γ '⊢ (list 'atom x ': y) ': t)
+     (judgement Γ x t)]))
+
+(define (write-eq x eq)
+  (cond
+    [(and (list? eq) (equal? (car eq) 'ty))
+     (match eq
+       [(list 'ty Γ e t) (list Γ '⊢ e ': t)]
+       [_ (raise "write-eq - fell off match")])]
+    [(judgement? eq)
+     (let [[Γ (judgement-env eq)]
+           [x (judgement-id eq)]
+           [t (judgement-type eq)]]
+       (list Γ '⊢ x ': t))]
+    [else
+     (list x '= eq)]))
+
+(define (get-constraints d)
+  (when (not (derivation? d))
+    (error 'get-constraints "expected a derivation, but found ~a" d))
+  (cond
+    [(eq? (derivation-name d) "t-equal")
+     (list (cadr (derivation-term d)))]
+    [(eq? (derivation-name d) "t-judge")
+     (list (cadr (derivation-term d)))]
+    [else
+     (apply append (map get-constraints (derivation-subs d)))]))
+
+
+;; ------------------------------------------------------------
+;; Unifications
+
+(define (print-unification unif port mode)
+  (hash-for-each (unification-judgements unif)
+                 (lambda (x j) (show-recur j port mode)))
+  (hash-for-each (unification-types unif)
+                 (lambda (x t) (show (format "  ~a = ~a\n" x t) port mode))))
+
+(define-struct unification (judgements types)
+  #:methods gen:custom-write
+  [(define write-proc print-unification)])
+
+(define (new-unification)
+  (unification (make-immutable-hash) (make-immutable-hash)))
+
+(define (insert-judgement unif x j)
+  (unification (hash-set (unification-judgements unif) x j)
+               (unification-types unif)))
+
+(define (insert-type unif x t)
+  (unification (unification-judgements unif)
+               (hash-set (unification-types unif) x t)))
+
+(define (lookup-judgement x unif)
+  (hash-lookup (unification-judgements unif) x))
+
+(define (lookup-type x unif)
+  (hash-lookup (unification-types unif) x))
+
+(define (unification-judgement-list unif)
+  (let [[hash (unification-judgements unif)]]
+    (map (lambda (x) (write-eq x (hash-ref hash x))) (hash-keys hash))))
+
+
+;; ------------------------------------------------------------
+;; Substitution
+
+(define (replace x t expr)
+  (define (recur expr)
+    (replace x t expr))
+  (cond
+    [(variable? expr) (if (equal? expr x) t expr)]
+    [(literal? expr)  expr]
+    [(list? expr)     (map recur expr)]
+    [(unification? expr)
+     (unification (map-hash recur (unification-judgements expr))
+                  (map-hash recur (unification-types expr)))]
+    [(judgement? expr)
+     (judgement (recur (judgement-env expr))
+                (recur (judgement-id expr))
+                (recur (judgement-type expr)))]
+    [else (raise "replace - fell off cond")]))
+
+(define (substitute unif t)
+  (define (recur t) (substitute unif t))
+  (cond
+    [(literal? t) t]
+    [(variable? t)
+     (match (lookup-type t unif)
+       [#f t]
+       [t2 t2])]
+    [(list? t)
+     (map recur t)]
+    [else (raise "substitute - fell off cond")]))
+
+(define (occurs? x t)
+  (define (recur t) (occurs? x t))
+  (cond
+    [(variable? t) (equal? x t)]
+    [(literal? t)  #f]
+    [(list? t)     (ormap recur t)]
+    [else          (raise "occurs? - fell off cond")]))
+
+
+;; ------------------------------------------------------------
+;; Unification Errors
+
+(define (unification-error x y)
+  (error 'unify "Could not unify `~a` with `~a`" x y))
+
+(define (occurs-error x t)
+  (error 'unify "Occurs check failure: `~a` occurs in `~a`" x t))
+
+
+;; ------------------------------------------------------------
+;; Resugaring
+
+(define (render-atoms t)
+  (cond
+    [(literal? t) t]
+    [(variable? t) t]
+    [(and (list? t) (not (empty? t)) (equal? 'atom (first t)))
+     (second t)]
+    [(list? t) (map render-atoms t)]
+    [else (raise "render-atoms - fell off cond")]))
+
+(define (make-sugar-rule name unif conclusion)
+  (derivation (render-atoms (substitute unif conclusion))
+              name
+              (map (lambda (eq) (derivation eq "assume" (list)))
+                   (unification-judgement-list unif))))
+
+(define-syntax-rule (resugar rule ty literals)
+  (parameterize ([fresh-vars (ds-rule-fresh rule)])
+    (let [[derivations (build-derivations (ty [] ,(ds-rule-rhs rule) _))]]
+      (when (not (eq? 1 (length derivations)))
+        (error 'derive "Expected exactly one derivation, but found ~a derivations" (length derivations)))
+      (let [[deriv (first derivations)]]
+        (parameterize [[language-literals literals]]
+          (display (get-constraints deriv)) (newline)
+          (let* [[unif (unify (map read-eq (get-constraints deriv))
+                              (new-unification))]
+                 [concl-type (fourth (derivation-term deriv))]
+                 [concl-env (second (derivation-term deriv))]
+                 [concl (list concl-env '⊢ (term ,(ds-rule-lhs rule)) ': concl-type)]]
+            (make-sugar-rule (ds-rule-name rule) unif concl)))))))
+
+
+;; ------------------------------------------------------------
+;; Unification
+
+(define (unify eqs unif)
+  (match eqs
+    [(list) unif]
+    [(cons (equation x y) eqs)
+     (unify eqs (equate x y unif))]
+    [(cons (? judgement? j) eqs)
+     (unify eqs (add-judgement j unif))]))
+
+(define (add-judgement j1 unif)
+  (let [[x (judgement-id j1)]]
+    (match (lookup-judgement x unif)
+      [#f (insert-judgement unif x j1)]
+      [j2 (equate-judgements j1 j2 unif)])))
+
+(define (equate-judgements j1 j2 unif)
+  (match* [j1 j2]
+    [[(judgement Γ1 x t1) (judgement Γ2 x t2)]
+     (equate-envs Γ1 Γ2 (equate t1 t2 unif))]))
+
+(define (equate-envs Γ1 Γ2 unif)
+  ; TODO: A simplification
+  (equate Γ1 Γ2))
+
+(define (equate x y unif)
+  (match* [x y]
+    [[(? variable? x) t]
+     ; Maintain the invarient that `subs` is a well-formed substitution:
+     ; it does not contain any of its variables in their definitions.
+     (when (and (not (variable? t)) (occurs? x t))
+       (occurs-error x t))
+     (match (lookup-type x unif)
+       [#f (insert-type (replace x t unif) x (substitute unif t))]
+       [t2 (equate t2 t unif)])]
+    ; symmetric case
+    [[t (? variable? x)]
+     (equate x t unif)]
+    ;; Potentially language-specific. This may work for now.
+    [[(? literal? x) y]
+     (when (not (equal? x y))
+       (unification-error x y))
+     unif]
+    ; symmetric case
+    [[x (? literal? y)]
+     (equate y x unif)]
+    ; Compound expressions
+    [[(list-rest xs) (list-rest ys)]
+     (when (not (equal? (length xs) (length ys)))
+       (unification-error xs ys))
+     (foldl equate unif xs ys)]
+    ; Do not match
+    [[_ _]
+     (unification-error x y)]))
+
+
+
+;; ------------------------------------------------------------
+;; Utility
+
+(define (boolean x)
+  (if x #t #f))
+
+(define (map-hash f hash)
+  (define (update key hash)
+    (hash-update hash key f))
+  (foldl update hash (hash-keys hash)))
+
+(define (hash-lookup hash key)
+  (if (hash-has-key? hash key)
+      (hash-ref hash key)
+      #f))
+
+(define (show x port mode)
+  (when mode (write-string x port)))
+
+(define (show-recur x port mode)
+  (case mode
+    [(#t) (write x)]
+    [(#f) (display x)]
+    [else (print x port mode)]))
+
+
